7499f49af73a8663798415ce95864ff49c3bba46
Mohamed <mohamedkamal159211@gmail.com>
2024-12-21 04:56:15 +0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -12,7 +12,7 @@ pipeline {
         stage('Checkout Code') {
             steps {
                 // Pull code from GitHub repository using Jenkins credentials
-                git credentialsId: "${GITHUB_CREDENTIALS}", url: "${GITHUB_REPO}", branch: 'main'
+                git credentialsId: "${GITHUB_CREDENTIALS}", url: "${GITHUB_REPO}", branch: 'main'  // Ensure 'main' is specified here
             }
         }
 
